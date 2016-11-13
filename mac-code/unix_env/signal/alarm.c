40915909f70f5aeb5df8e9e564ed90519c41e4bc
18682303408@163.com <18682303408@163.com>
2016-11-13 13:17:14 +0800

    mac signal

---
+++ mac-code/unix_env/signal/alarm.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+#include <unistd.h>
+#include <pwd.h>
+
+
+static char *get_username(void)
+{
+#if defined(__APPLE__) && defined(__MACH__)
+	return "weiliu";
+#elif defined(UNIX)
+	return "parallels";
+#else
+	return NULL;
+#endif
+}
+
+
+static void do_func(int signo)
+{
+	struct passwd *rootptr = NULL;
+
+	printf("in signal handler\n");
+	if ((rootptr = getpwnam("root")) == NULL)
+	{
+		perror("getpwnam root info error");
+		exit(EXIT_FAILURE);
+	}
+
+	alarm(1);
+}
+
+
+int main(int argc, char *argv[])
+{
+	struct passwd *ptr = NULL;
+
+	signal(SIGALRM, do_func);
+	alarm(1);
+
+	for (;;)
+	{
+		if ((ptr = getpwnam(get_username())) == NULL)
+		{
+			perror("getpwnam error");
+			exit(EXIT_FAILURE);
+		}
+
+		if (strcmp(ptr->pw_name, get_username()) != 0)
+		{
+			printf("return value corrupted!, pw_name = %s\n", ptr->pw_name);
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
