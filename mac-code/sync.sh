3ddff8fc29a0c2aa28753f580fdb163d88ccf59c
18682303408@163.com <18682303408@163.com>
2016-11-20 16:18:45 +0800

    mac test

---
+++ mac-code/sync.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+##########################################################################
+# local and git sync
+##########################################################################
+
+workdir=`pwd`
+curtm=`date "+%Y%m%d %H%M%S"`
+
+
+function git_add()
+{
+	local addfile=${1}
+
+	git add "${addfile}"
+}
+
+function git_commit()
+{
+	git commit -m "${workdir} ${curtm}"
+}
+
+function git_pull()
+{
+	git pull
+}
+
+function git_push()
+{
+	git push
+}
+
+
+git_pull
+ls -al -R ${workdir} | egrep '^-' | awk '{print $9}' | egrep '.*(\.(c|C|c(pp|\+\+)|sh|php|h|python))$' | while read file
+do
+	git_add ${file}
+done
+
+git_commit
+git_push
