2e40a26512485a6ee145718afb59221aa79e2cde
18682303408@163.com <18682303408@163.com>
2016-12-19 00:24:04 +0800

    bogon Mon Dec 19 00:24:04 CST 2016

--- mac-code/unix_env/thread/try_release_lock.c
+++ mac-code/unix_env/thread/try_release_lock.c
@@ -6,6 +6,10 @@
 #include <pthread.h>
 
 
+static pthread_mutex_t locka = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t lockb = PTHREAD_MUTEX_INITIALIZER;
+
+
 int main(int argc, char *argv[])
 {
 	return EXIT_SUCCESS;
