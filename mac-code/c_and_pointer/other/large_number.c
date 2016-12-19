0b9102c57a835cbac2ad4cc166a382035c9c2d5c
18682303408@163.com <18682303408@163.com>
2016-12-20 00:36:05 +0800

    bogon Tue Dec 20 00:36:05 CST 2016

--- mac-code/c_and_pointer/other/large_number.c
+++ mac-code/c_and_pointer/other/large_number.c
@@ -5,9 +5,20 @@
 
 #define print_msg		printf("Enter multiplier: ")
 
+static void clear(void)
+{
+	scanf();
+	scanf();
+}
+
 static void enter_mul(unsigned long long *ptr)
 {
-	for (print_msg; scanf("%llu", ptr) != 1; )
+	for (print_msg; scanf("%llu", ptr) != 1; print_msg)
+	{
+		printf("Enter invalid\n");
+
+		clear();
+	}
 }
 
 int main(int argc, char *argv[])
