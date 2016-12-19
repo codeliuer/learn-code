7d60febfad6890a1a39edb58b10c98901eb1029b
18682303408@163.com <18682303408@163.com>
2016-12-20 00:34:04 +0800

    bogon Tue Dec 20 00:34:04 CST 2016

--- mac-code/c_and_pointer/other/large_number.c
+++ mac-code/c_and_pointer/other/large_number.c
@@ -3,7 +3,15 @@
 #include <string.h>
 
 
+static void enter_mul(unsigned long long *ptr)
+{
+}
+
 int main(int argc, char *argv[])
 {
+	unsigned long long mul = 0;
+
+	enter_mul(&mul);
+
 	return EXIT_SUCCESS;
 }
