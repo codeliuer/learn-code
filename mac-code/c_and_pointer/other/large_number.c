929aa878ff964d4bfd1cc433233fe3ae8b447d1f
18682303408@163.com <18682303408@163.com>
2016-12-20 02:25:06 +0800

    bogon Tue Dec 20 02:25:06 CST 2016

--- mac-code/c_and_pointer/other/large_number.c
+++ mac-code/c_and_pointer/other/large_number.c
@@ -3,7 +3,13 @@
 #include <string.h>
 
 
+#define BASE_DATA		(10)
+#define MAX_RANGE		(10)
 #define print_msg		printf("Enter multiplier: ")
+
+
+typedef unsigned long long	u64int_t;
+typedef unsigned int		u32int_t;
 
 static void clear_stdin_buffer(void)
 {
@@ -11,7 +17,7 @@ static void clear_stdin_buffer(void)
 	scanf("%*c");
 }
 
-static void enter_mul(unsigned int *ptr)
+static void enter_mul(u32int_t *ptr)
 {
 	for (print_msg; scanf("%u", ptr) != 1; print_msg)
 	{
@@ -21,22 +27,85 @@ static void enter_mul(unsigned int *ptr)
 	}
 }
 
-static unsigned int *get_buffer(unsigned int mul)
+#define free_buffer(array)		{free(array); array = NULL;}
+
+static u32int_t get_zero(unsigned int max)
 {
-	int *array = malloc(mul * 4);
+	u32int_t n = 0;
+
+	while (max /= BASE_DATA)
+	{
+		n += 1;
+	}
+
+	return n;
 }
 
-static void factorial(unsigned int mul)
+static u32int_t *get_buffer(u32int_t mul)
 {
-	unsigned int *array = get_buffer(mul);
+	u32int_t bits = get_zero(mul);
+
+	u32int_t *array = malloc((bits*mul) * 4);
+
+	if (array == NULL)
+	{
+		perror("malloc:");
+		exit(EXIT_FAILURE);
+	}
+
+	return array;
+}
+
+static void calculate(u32int_t *array, u32int_t mul)
+{
+	u32int_t n = 0;
+	u32int_t i = 0, j = 0, m = 0;
+	u64int_t bigdigit = 0, mod = 0;
+
+	m = 0, array[m] = (1<mul) ? 1 : 0;
+	for (i = 2; i <= mul; i++)
+	{
+		bigdigit = 0, mod = 0;
+
+		for (j = 0; j <= m; j++)
+		{
+			bigdigit = array[j] * i + mod;
+			array[j] = bigdigit % MAX_RANGE;
+			mod = bigdigit / MAX_RANGE;
+		}
+
+		while (mod > 0)
+		{
+			array[++m] = mod % MAX_RANGE;
+			mod /= MAX_RANGE;
+		}
+	}
+
+	n = get_zero(MAX_RANGE);
+
+	printf("%d", array[m]);
+	while (m--)
+	{
+		printf("%0*d", n, array[m]);
+	}
+	printf("\n");
+}
+
+static void factorial(u32int_t mul)
+{
+	u32int_t *array = get_buffer(mul);
+
+	calculate(array, mul);
+
+	free_buffer(array);
 }
 
 int main(int argc, char *argv[])
 {
-	unsigned int mul = 0;
+	u32int_t mul = 0;
 
 	enter_mul(&mul);
-
+	
 	factorial(mul);
 
 	return EXIT_SUCCESS;
