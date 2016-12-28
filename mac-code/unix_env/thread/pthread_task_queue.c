ef80349483c02ba4de96f4798da06864b456201e
18682303408@163.com <18682303408@163.com>
2016-12-29 00:09:08 +0800

    bogon Thu Dec 29 00:09:08 CST 2016

---
+++ mac-code/unix_env/thread/pthread_task_queue.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <pthread.h>
+
+
+typedef struct task 
+{
+} task_t
+
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
