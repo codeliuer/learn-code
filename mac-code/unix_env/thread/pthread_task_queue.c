29ba885b52c1d4c6c69b4d6cb4edc8c75654bb3e
18682303408@163.com <18682303408@163.com>
2016-12-29 00:11:07 +0800

    bogon Thu Dec 29 00:11:07 CST 2016

--- mac-code/unix_env/thread/pthread_task_queue.c
+++ mac-code/unix_env/thread/pthread_task_queue.c
@@ -6,8 +6,19 @@
 #include <pthread.h>
 
 
+typedef struct job
+{
+    struct job          *j_next;
+    struct job          *j_prev;
+    
+} job_t;
+
+
 typedef struct task 
 {
+    job_t               *t_head;
+    job_t               *p_tail;
+    pthread_rwlock_t    tid;
 } task_t
 
 
