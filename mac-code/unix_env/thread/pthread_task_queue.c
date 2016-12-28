3ba14bc2e8efe25247d8a9a050c76f4abde7453e
18682303408@163.com <18682303408@163.com>
2016-12-29 00:51:05 +0800

    bogon Thu Dec 29 00:51:05 CST 2016

--- mac-code/unix_env/thread/pthread_task_queue.c
+++ mac-code/unix_env/thread/pthread_task_queue.c
@@ -48,11 +48,6 @@ static int insert_task(task_t *queue, job_t *job)
 
     job->j_next = queue->t_head;
     job->j_prev = NULL;
-    queue->t_head = job;
-    if (queue->t_head != NULL)
-    {
-        queue->t_tail = job;
-    }
 
     pthread_rwlock_unlock(&queue->t_lock);
 }
