a3ca685e714a6d95e08ee5da4820b9a2ae1ae844
liuwei <18682303408@163.com>
2016-12-31 12:31:05 -0500

    centos Sat Dec 31 12:31:05 EST 2016

--- centos/unix_env/11/barrier.c
+++ centos/unix_env/11/barrier.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 
     for (i = 0; i < count; i++)
     {
-        pthread_create(&thid[i], NULL, thread, data);
+        pthread_create(&thid[i], NULL, thread, data+);
     }
 
     pthread_barrier_destroy(&barrier);
