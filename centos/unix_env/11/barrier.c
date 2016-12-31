bd955f5c1f8c2575d571a80572f40b01341b3c2e
liuwei <18682303408@163.com>
2016-12-31 12:01:04 -0500

    centos Sat Dec 31 12:01:04 EST 2016

--- centos/unix_env/11/barrier.c
+++ centos/unix_env/11/barrier.c
@@ -5,13 +5,17 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define THREAD_COUNT            (8)
 
 static pthread_barrier_t barrier;
 
 
 int main(int argc, char *argv[])
 {
-    pthread_barrier_init(&barrier, NULL);
+    int count = THREAD_COUNT;
+
+    pthread_barrier_init(&barrier, NULL, count);
+    pthread_barrier_
 
     return EXIT_SUCCESS;
 }
