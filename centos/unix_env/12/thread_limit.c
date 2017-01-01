4087708d486374595ef2683986741bf6d8a6a689
liuwei <18682303408@163.com>
2017-01-01 08:45:03 -0500

    centos Sun Jan  1 08:45:03 EST 2017

--- centos/unix_env/12/thread_limit.c
+++ centos/unix_env/12/thread_limit.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[])
 {
     printf("thread quit max number = %ld\n", sysconf(_SC_THREAD_DESTRUCTOR_ITERATIONS));
     printf("able create key max = %ld\n", sysconf(_SC_THREAD_KEYS_MAX));
-    printf("thread stack min = %ld\n", sysconf);
+    printf("thread stack min = %ld\n", sysconf(_SC_THREAD_STACK_MIN));
+    printf("thread max number = %ld\n", sysconf(_SC_THREAD_THREADS_MAX));
 
     return EXIT_SUCCESS;
 }
