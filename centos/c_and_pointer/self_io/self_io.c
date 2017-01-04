43ff6b574006ed6e6688ffe265990213651f5b59
liuwei <18682303408@163.com>
2017-01-03 22:12:04 -0500

    centos Tue Jan  3 22:12:04 EST 2017

--- centos/c_and_pointer/self_io/self_io.c
+++ centos/c_and_pointer/self_io/self_io.c
@@ -10,8 +10,11 @@ int self_print(const char *file, const char *func, int line, const char *format,
 {
     va_list ap;
 
-    va_start(ap, format);
     vprintf("%s(%s)[%d]", file, func, line);
+
+    va_start(ap, format);
+    vprintf(format, ap);
+    va_end(ap);
 }
 
 
