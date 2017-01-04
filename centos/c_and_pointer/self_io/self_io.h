a3798f0e9e9e8212669ed05db6f3aa7c5bb8a69d
liuwei <18682303408@163.com>
2017-01-03 22:14:04 -0500

    centos Tue Jan  3 22:14:04 EST 2017

---
+++ centos/c_and_pointer/self_io/self_io.h
@@ -0,0 +1,37 @@
+#ifndef __SELF_IO__
+#define __SELF_IO__
+
+#define printinfo(format, ...)      self_print(__FILE__, __func__, __LINE__, format, ##__VA_ARGS__)
+#define fprintinfo(io, format, ...) self_fprint(__FILE__, __func__, __LINE__, io, format, ##__VA_ARGS__)
+
+
+int self_print(const char *file, const char *func, int line, const char *format, ...)
+{
+    va_list ap;
+    int bytes = 0;
+
+    vprintf("%s(%s)[%d]", file, func, line);
+
+    va_start(ap, format);
+    bytes = vprintf(format, ap);
+    va_end(ap);
+
+    return bytes;
+}
+
+
+int self_fprint(const char *file, const char *func, int line, FILE *stream, const char *format, ...)
+{
+    va_list ap;
+    int bytes = 0;
+
+    vfprintf(stream, "%s(%s)[%d]", file, func, line);
+
+    va_start(ap, format);
+    bytes = vfprintf(stream, format, ap);
+    va_end(ap);
+
+    return bytes;
+}
+
+#endif //__SELF_IO__
