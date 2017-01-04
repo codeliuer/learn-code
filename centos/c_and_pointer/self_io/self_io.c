971bc5627585f90e4436659312b988076da76502
liuwei <18682303408@163.com>
2017-01-03 22:07:04 -0500

    centos Tue Jan  3 22:07:04 EST 2017

--- centos/c_and_pointer/self_io/self_io.c
+++ centos/c_and_pointer/self_io/self_io.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 
-#define printinfo(format, ...)  self_print(__FILE__, __func__, __LINE__, format, ##__VA_ARGS)
+#define printinfo(format, ...)  self_print(__FILE__, __func__, __LINE__, format, ##__VA_ARGS__)
 
 
-int self_print()
+int self_print(const char *file, const char *func, int line, const char *format, ...)
 {
 }
 
