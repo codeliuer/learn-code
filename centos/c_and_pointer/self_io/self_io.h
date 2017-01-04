dbb46e758f0e48c820c5b65e412736a6d1ffafbe
liuwei <18682303408@163.com>
2017-01-03 22:20:04 -0500

    centos Tue Jan  3 22:20:04 EST 2017

--- centos/c_and_pointer/self_io/self_io.h
+++ centos/c_and_pointer/self_io/self_io.h
@@ -6,7 +6,7 @@
 #define fprintinfo(io, format, ...) self_fprint(__FILE__, __func__, __LINE__, io, format, ##__VA_ARGS__)
 
 
-extern int printinfo(const char *format, ...);
-extern int fprintinfo(FILE *stream, const char *format, ...);
+extern int self_print(const char *format, ...);
+extern int self_fprint(FILE *stream, const char *format, ...);
 
 #endif //__SELF_IO__
