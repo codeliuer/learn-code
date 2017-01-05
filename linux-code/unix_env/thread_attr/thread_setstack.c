38de9869c9d7a4c258f855c1c49a77adaf7b1248
liuwei <18682303408@163.com>
2017-01-06 03:00:08 +0800

    ubuntu Fri Jan  6 03:00:08 HKT 2017

---
+++ linux-code/unix_env/thread_attr/thread_setstack.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <pthread.h>
+
+
+int main(int argc, char *argv[])
+{
+    pthread_t thid;
+    pthread_attr_t attr;
+    void *base = (void *)malloc(PTHREAD_STACK_MIN + 0x4000);
+
+    if (pthread_attr_init(&attr) != 0)
+    {
+        fprintf(stderr, "thread attribute init failure\n");
+        return EXIT_FAILURE;
+    }
+
+    if (pthread_attr_setstack(&attr, base, PTHREAD_STACK_MIN + 0x4000) != 0)
+    {
+        fprintf(stderr, "thread attrabute stack set failure\n");
+        return EXIT_FAILURE;
+    }
+
+    if (pthread_create(&thid, &attr, thread_func, NULL) != 0)
+    {
+        fprintf(stderr, "create thread failure\n");
+        return EXIT_FAILURE;
+    }
+
+    sleep (1);
+
+    return EXIT_SUCCESS;
+}
