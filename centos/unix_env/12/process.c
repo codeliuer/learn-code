7fe018c3191f3691552309caf73e6ea297ebe91e
liuwei <18682303408@163.com>
2017-01-09 12:00:07 -0500

    centos Mon Jan  9 12:00:07 EST 2017

---
+++ centos/unix_env/12/process.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <self_io.h>
+#include <assert.h>
+
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+#define SHARED_FILE         "shared"
+
+
+static int getfd(void)
+{
+    int fd = 0;
+
+    fd = open(SHARED_FILE, O_RDWR);
+    if (fd < 0)
+    {
+        fprintinfo(stderr, "open %s file failure\n", SHARED_FILE);
+        assert(fd < 0);
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    int fd = 0;
+    pthread_attr_t *attr = NULL;
+
+    printinfo("main function\n");
+
+    attr = getspace();
+
+    return EXIT_SUCCESS;
+}
