e05118a827659b2af704eaee02750d4b6e21bd50
liuwei <18682303408@163.com>
2017-01-10 14:36:06 -0500

    centos Tue Jan 10 14:36:06 EST 2017

--- centos/unix_env/12/mmap.c
+++ centos/unix_env/12/mmap.c
@@ -27,6 +27,7 @@ int main(int argc, char *argv[])
         fprintinfo(stderr, "open %s file failure\n", MMAP_FILE);
         return EXIT_FAILURE;
     }
+    printinfo("open filename: %s, fd = %d\n", MMAP_FILE, fd);
 
     printinfo("stat filename: %s\n", MMAP_FILE);
     retcode = stat(MMAP_FILE, &st);
@@ -36,7 +37,7 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    printinfo("mmap fd: %d, size = %ld, attribute: (prot = %d, shared = %d\n", fd, st.st_size, PROT_READ|PROT_WRITE, MAP_SHARED);
+    printinfo("mmap fd: %d, size = %ld, attribute: (prot = %d, shared = %d)\n", fd, st.st_size, PROT_READ|PROT_WRITE, MAP_SHARED);
     addr = mmap(NULL, st.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
     if (addr == NULL)
     {
