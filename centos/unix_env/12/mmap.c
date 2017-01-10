6bb3d993fe618ea6d51caefa344c27830ca6b32b
liuwei <18682303408@163.com>
2017-01-09 20:31:47 -0500

    centos Mon Jan  9 20:31:47 EST 2017

--- centos/unix_env/12/mmap.c
+++ centos/unix_env/12/mmap.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <self_io.h>
 
 #include <unistd.h>
 #include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 
 #define Mmap_FILE           "shared"
@@ -11,6 +15,15 @@
 
 int main(int argc, char *argv[])
 {
+    int fd = 0;
+    void *addr = NULL;
+
+    fd = open(MMAP_FILE, O_RDWR);
+    if (fd < 0)
+    {
+        fprintinfo(stderr, "open %s file failure\n", MMAP_FILE);
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
