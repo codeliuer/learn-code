a0d562d54edeb0f1d0b93e203d4a412e25af0333
liuwei <18682303408@163.com>
2017-01-09 20:30:06 -0500

    centos Mon Jan  9 20:30:06 EST 2017

---
+++ centos/unix_env/12/mmap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/mman.h>
+
+
+#define Mmap_FILE           "shared"
+
+
+int main(int argc, char *argv[])
+{
+
+    return EXIT_SUCCESS;
+}
