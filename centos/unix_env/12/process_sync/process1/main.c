e60a2efae66de24e97794f151b513238421204e3
liuwei <18682303408@163.com>
2017-01-12 12:00:06 -0500

    centos Thu Jan 12 12:00:06 EST 2017

--- centos/unix_env/12/process_sync/process1/main.c
+++ centos/unix_env/12/process_sync/process1/main.c
@@ -5,8 +5,20 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#include "../api.h"
+
 
 int main(int argc, char *argv[])
 {
+    pthread_mutex_t *addr = mapaddr_init(sizeof(*addr), MAP_PROCESS_SHARED);
+    if (addr == NULL)
+    {
+        printinfo("get thread mutex space failure\n");
+        return EXIT_FAILURE;
+    }
+
+    pthread_mutexattr_init(addr);
+    pthread_mutexattr_setpshared(addr, );
+
     return EXIT_SUCCESS;
 }
