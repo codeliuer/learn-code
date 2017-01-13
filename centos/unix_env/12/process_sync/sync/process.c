e3c8da23d04342285be6c55e66fe7d713868648d
liuwei <18682303408@163.com>
2017-01-13 12:00:07 -0500

    centos Fri Jan 13 12:00:07 EST 2017

---
+++ centos/unix_env/12/process_sync/sync/process.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+
+
+int main(int argc, char *argv)
+{
+    return EXIT_SUCCESS;
+}
