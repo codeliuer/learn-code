9c3e296c3fa555faaf625c135b6439ba72d1c706
liuwei <18682303408@163.com>
2017-01-13 12:09:06 -0500

    centos Fri Jan 13 12:09:06 EST 2017

--- centos/unix_env/12/process_sync/sync/process.c
+++ centos/unix_env/12/process_sync/sync/process.c
@@ -13,5 +13,10 @@ int main(int argc, char *argv)
     pthread_mutex_t mutex;
     pthread_mutexattr_t attr;
 
+    pthread_mutexattr_init(&attr);
+    pthread_mutex_attr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
+
+    pthread_mutex_init(&mutex, &attr);
+
     return EXIT_SUCCESS;
 }
