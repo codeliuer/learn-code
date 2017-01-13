8e6c93be4ebb35be12fc72f9b44d6a6ff40f3303
liuwei <18682303408@163.com>
2017-01-13 12:20:16 -0500

    centos Fri Jan 13 12:20:16 EST 2017

--- centos/unix_env/12/process_sync/sync/lock.c
+++ centos/unix_env/12/process_sync/sync/lock.c
@@ -12,24 +12,14 @@
 int main(int argc, char *argv)
 {
     int fd = open("shared", O_RDWR);
-    pthread_mutexattr_t attr;
     pthread_mutex_t *mutex = mmap(NULL, sizeof(*mutex), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
-    pthread_mutexattr_init(&attr);
-    pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
-
-    pthread_mutex_init(mutex, &attr);
-
-    pthread_mutexattr_destroy(&attr);
-
+    printf("try to get lock\n");
     pthread_mutex_lock(mutex);
     printf("process pid = %d get lock\n", getpid());
-    sleep(100);
+    sleep(1);
     printf("free lock\n");
     pthread_mutex_unlock(mutex);
-
-
-    pthread_mutex_destroy(mutex);
 
     return EXIT_SUCCESS;
 }
--- centos/unix_env/12/process_sync/sync/process.c
+++ centos/unix_env/12/process_sync/sync/process.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv)
     pthread_mutex_unlock(mutex);
 
 
-    pthread_mutex_destroy(mutex);
+//    pthread_mutex_destroy(mutex);
 
     return EXIT_SUCCESS;
 }
