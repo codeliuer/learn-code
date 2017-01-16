b8e9ae611e7de17708cf9b20b4d7189fb827c1f7
liuwei <18682303408@163.com>
2017-01-16 12:59:05 -0500

    centos Mon Jan 16 12:59:05 EST 2017

--- centos/unix_env/12/register.c
+++ centos/unix_env/12/register.c
@@ -10,5 +10,11 @@
 
 int main(int argc, char *argv[])
 {
+    int retcode = 0;
+    pthread_mutexattr_t attr = {0};
+    pthread_mutex_t lock = {0};
+
+    pthread_mutexattr_init(&attr);
+
     return EXIT_SUCCESS;
 }
