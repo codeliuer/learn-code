53b7ae41cbfd6aff110752f6802ef8abf4252e53
liuwei <18682303408@163.com>
2017-01-19 03:12:04 +0800

    ubuntu Thu Jan 19 03:12:04 HKT 2017

---
+++ centos/unix_env/12/hot_water.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <unistd.h>
+#include <pthread.h>
+
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
