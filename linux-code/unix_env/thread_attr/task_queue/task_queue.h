f9672ea2199142899edc40e931ae32b44f902478
liuwei <18682303408@163.com>
2017-01-21 00:00:05 +0800

    ubuntu Sat Jan 21 00:00:05 HKT 2017

---
+++ linux-code/unix_env/thread_attr/task_queue/task_queue.h
@@ -0,0 +1,14 @@
+/****************************************************************
+ * task queue set and get
+ ***************************************************************/
+
+#ifndef __TASK_QUEUE_H__
+#define __TASK_QUEUE_H__
+
+
+typedef struct task
+{
+} task_t;
+
+
+#endif //__TASK_QUEUE_H__
