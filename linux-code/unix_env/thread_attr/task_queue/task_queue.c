48f1f1a3ae2be15df6f789b17acc76759f943b38
liuwei <18682303408@163.com>
2017-01-21 03:00:05 +0800

    ubuntu Sat Jan 21 03:00:05 HKT 2017

---
+++ linux-code/unix_env/thread_attr/task_queue/task_queue.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <pthread.h>
+
+#include "task_queue.h"
+
+
+typedef struct task_queue
+{
+    task_t *task;
+    int count;
+    pthread_mutex_t lock;
+} task_queue_t;
+
+
+static pthread_mutex_t global = PTHREAD_MUTEX_INITIALIZER;
+
+static task_queue_t *queue = NULL;
+
+int task_init(void)
+{
+    pthread_mutex_lock(&global);
+
+    if (queue != NULL)
+    {
+        fprintf(stderr, "task inited\n");
+        pthread_mutex_unlock(&global);   
+
+        return TASK_ERRCODE_INITED;
+    }
+
+    queue = (task_queue_t *)malloc(sizeof(*queue));
+    if (queue == NULL)
+    {
+        fprintf(stderr, "malloc failure\n");
+        pthread_mutex_unlock(&global);
+
+        return TASK_ERRCODE_ALLOC;
+    }
+
+    if (pthread_mutex_init())
+    {
+    }
+
+    pthread_mutex_unlock(&global);
+}
+
+
+int task_destroy(void)
+{
+}
--- linux-code/unix_env/thread_attr/task_queue/task_queue.h
+++ linux-code/unix_env/thread_attr/task_queue/task_queue.h
@@ -6,9 +6,21 @@
 #define __TASK_QUEUE_H__
 
 
+#define TASK_ERRCODE_SUCCESS        (0)
+#define TASK_ERRCODE_FAIL           (1)
+#define TASK_ERRCODE_INITED         (2)
+
+
 typedef struct task
 {
+    void *(*task)(void *);
+    void *arg;
+    struct task *next;
 } task_t;
+
+
+extern int task_init(void);
+extern int task_destroy(void);
 
 
 #endif //__TASK_QUEUE_H__
