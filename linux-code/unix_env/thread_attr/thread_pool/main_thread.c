cc81e8e9dedd0324bfc169f6fdb5cf26eefa4c10
liuwei <18682303408@163.com>
2017-01-22 00:33:04 +0800

    ubuntu Sun Jan 22 00:33:04 HKT 2017

--- linux-code/unix_env/thread_attr/thread_pool/main_thread.c
+++ linux-code/unix_env/thread_attr/thread_pool/main_thread.c
@@ -19,12 +19,14 @@ static void *func(void *arg)
 int main(int argc, char *argv[])
 {
     int i = 0;
-    struct task_struct task = {func, 0};
+    struct task_struct task;
 
     thread_pool_init(10);
 
     for (i = 0; i < MAX; i++)
     {
+        pthread_mutex_lock(&lock);
+        pthread_mutex_unlock(&lock);
     }
 
     thread_pool_destroy();
