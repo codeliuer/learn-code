279ec00cc9e60e8c063974788852744fcc40c4eb
liuwei <18682303408@163.com>
2017-01-22 00:31:05 +0800

    ubuntu Sun Jan 22 00:31:05 HKT 2017

--- linux-code/unix_env/thread_attr/thread_pool/main_thread.c
+++ linux-code/unix_env/thread_attr/thread_pool/main_thread.c
@@ -6,8 +6,17 @@
 #include "thread_pool.h"
 
 
+static void *func(void *arg)
+{
+    int data = *(int *)arg;
+
+    printf("data = %d\n", data);
+}
+
 int main(int argc, char *argv[])
 {
+    struct task_struct task;
+
     thread_pool_init(10);
 
     thread_pool_destroy();
