6c71c19c0d688e12e6ea40e0f74fff307e64b98e
liuwei <18682303408@163.com>
2017-01-22 00:51:05 +0800

    ubuntu Sun Jan 22 00:51:05 HKT 2017

--- linux-code/unix_env/thread_attr/test_cond/thread_cond.c
+++ linux-code/unix_env/thread_attr/test_cond/thread_cond.c
@@ -1,1 +1,12 @@
-#Include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <pthread.h>
+
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
