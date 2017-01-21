55386cdf3678701d2b9891bcfaa624a73fc99689
liuwei <18682303408@163.com>
2017-01-22 00:26:06 +0800

    ubuntu Sun Jan 22 00:26:06 HKT 2017

--- linux-code/unix_env/thread_attr/thread_pool/thread_pool.h
+++ linux-code/unix_env/thread_attr/thread_pool/thread_pool.h
@@ -1,17 +1,6 @@
-/******************************************************
- * thread task
- ******************************************************/
-
 #ifndef __THREAD_POOL_H__
 #define __THREAD_POOL_H__
 
-
-extern int task_init(void);
-extern int task_destroy(void);
-
-extern int task_insert(void *);
-extern int task_append(void *);
-extern void *task_remove(void);
 
 
 #endif //__THREAD_POOL_H__
