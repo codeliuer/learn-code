c7fcaf158d30e9d93cb9a32306537f466f24780c
liuwei <18682303408@163.com>
2017-01-22 00:11:05 +0800

    ubuntu Sun Jan 22 00:11:05 HKT 2017

--- linux-code/unix_env/thread_attr/thread_pool/thread_pool.c
+++ linux-code/unix_env/thread_attr/thread_pool/thread_pool.c
@@ -15,8 +15,13 @@ static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 static void *thread_task(void *arg)
 {
+    struct task_struct *task = NULL;
+
     forever_true()
     {
-        pthread_mutex_lock();
+        pthread_mutex_lock(&lock);
+        while ()
+        {
+        }
     }
 }
