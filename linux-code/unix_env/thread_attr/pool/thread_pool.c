2032782eb9c051b7701e99a5020f1bdfe4bcf064
liuwei <18682303408@163.com>
2017-01-24 12:06:05 +0800

    ubuntu Tue Jan 24 12:06:05 HKT 2017

--- linux-code/unix_env/thread_attr/pool/thread_pool.c
+++ linux-code/unix_env/thread_attr/pool/thread_pool.c
@@ -5,6 +5,18 @@
 #include <pthread.h>
 
 
+static void thread_pool_init(int max)
+{
+    int i = 0;
+    pthread_t thid;
+
+    for (i = 0; i < max; i++)
+    {
+        pthread_create();
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
     return EXIT_SUCCESS;
