b343ce12e9a2ad04a69dbdc1e2e349f98785ae40
liuwei <18682303408@163.com>
2017-01-24 12:08:06 +0800

    ubuntu Tue Jan 24 12:08:06 HKT 2017

--- linux-code/unix_env/thread_attr/pool/thread_pool.c
+++ linux-code/unix_env/thread_attr/pool/thread_pool.c
@@ -8,14 +8,20 @@
 static void thread_pool_init(int max)
 {
     int i = 0;
-    int num = 0;
+    int suc_num = 0;
+    int fal_num = 0;
     pthread_t thid;
 
     for (i = 0; i < max; i++)
     {
         if (pthread_create(&thid, NULL, thread_pool, NULL) != 0)
         {
+            fal_num += 1;
+            fprintf(stderr, "thread init failure, number = %d\n", fal_num);
+            continue;
         }
+
+        suc_num += 1;
     }
 }
 
