1eeb93966adc7d0650d3b6004fa16fb951a583e5
liuwei <18682303408@163.com>
2017-01-26 14:00:09 -0500

    centos Thu Jan 26 14:00:09 EST 2017

---
+++ linux-code/unix_env/thread_attr/getenv_r.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern char **environ;
+
+static pthread_mutex_t lock;
+
+static void thread_once(void)
+{
+    pthread_mutexattr_t attr;
+
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_setpshared(&attr, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutex_init(&lock, &attr);
+    pthread_mutex_destroy(&attr);
+}
+
+
+char *get_env_r(const char *name)
+{
+    int i = 0;
+    char *local = NULL;
+    size_t len = strlen(name);
+
+    pthread_once(PTHREAD_ONCE_INIT, thread_once);
+
+    pthread_mutex_lock(&lock);
+    for (i = 0; environ[i] != NULL; i++)
+    {
+        if (environ[i][len] == '=' 
+            && strncmp(name, environ[i], len) == 0)
+        {
+            pthread_mutex_unlock(&lock);
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
