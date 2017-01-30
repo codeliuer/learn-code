333a9d4c5d989fb6b7bdea2f5ea51707fc0fa976
liuwei <18682303408@163.com>
2017-01-31 00:45:07 +0800

    ubuntu Tue Jan 31 00:45:07 HKT 2017

--- linux-code/unix_env/thread_attr/errno.c
+++ linux-code/unix_env/thread_attr/errno.c
@@ -1,6 +1,17 @@
-#include <>stdio.h
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <unistd.h>
 #include <pthread.h>
+
+
+int pthread_private_val;
+
+
+int main(int argc, char *argv[])
+{
+
+
+    return EXIT_SUCCESS;
+}
