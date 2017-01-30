26fa4a49476f54f265c292c2605531315155e169
liuwei <18682303408@163.com>
2017-01-31 00:30:08 +0800

    ubuntu Tue Jan 31 00:30:08 HKT 2017

--- linux-code/unix_env/thread_attr/pthread_key.c
+++ linux-code/unix_env/thread_attr/pthread_key.c
@@ -14,11 +14,19 @@ int main(int argc, char *argv[])
     pthread_key_t key;
 
     pthread_key_create(&key, free);
+
+    tmp = malloc(sizeof(int));
+    *(unsigned int *)tmp = 4;
+    pthread_setspecific(key, tmp);
+
     value = pthread_getspecific(key);
     if (value == NULL)
     {
         fprintf(stderr, "thread private key not relation\n");
 
+    }
+    else
+    {
     }
 
     return EXIT_SUCCESS;
