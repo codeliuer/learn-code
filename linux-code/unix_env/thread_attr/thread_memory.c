ec56c217cd9025af196cebc4231465b103e97094
liuwei <18682303408@163.com>
2017-02-04 00:55:06 +0800

    ubuntu Sat Feb  4 00:55:06 HKT 2017

--- linux-code/unix_env/thread_attr/thread_memory.c
+++ linux-code/unix_env/thread_attr/thread_memory.c
@@ -10,7 +10,6 @@ static pthread_key_t  thread_key;
 static pthread_once_t once = PTHREAD_ONCE_INIT;
 
 
-int main(int argc, char *argv[])
+void *make_space(size_t len)
 {
-    return EXIT_SUCCESS;
 }
