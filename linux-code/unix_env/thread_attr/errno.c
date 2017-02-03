b2b97eee7a160efb482d74d5124f9c50f15599ad
liuwei <18682303408@163.com>
2017-02-04 00:00:07 +0800

    ubuntu Sat Feb  4 00:00:07 HKT 2017

--- linux-code/unix_env/thread_attr/errno.c
+++ linux-code/unix_env/thread_attr/errno.c
@@ -6,60 +6,20 @@
 #include <pthread.h>
 
 
-#define pthread_private_val (*__pthread_private_val())
-
-static int *__pthread_private_val(void);
-
-static pthread_once_t initflag = PTHREAD_ONCE_INIT;
+#define private_val (*__private_localtion())
 
 
-static pthread_key_t key;
+static int private_val;
 
-static void only_once(void)
+static pthread_key_t thread_key;
+
+
+static int *__private_localtion(void)
 {
-    int *val = (int *)malloc(sizeof(*val));
-
-    printf("here\n");
-
-    pthread_key_create(&key, free);
-
-    pthread_setspecific(key, val);
 }
 
-
-static int *__pthread_private_val(void)
-{
-    int *val = NULL;
-
-    pthread_once(&initflag, only_once);
-
-    val = (int *)pthread_getspecific(key);
-    printf("thread id = %ld, val = %p\n", pthread_self(), val);
-
-    return val;
-}
-
-static void *thread_func(void *arg)
-{
-    pthread_private_val = 10;
-
-    printf("pthread_private_val = %d\n", pthread_private_val);
-
-    pthread_exit(NULL);
-}
 
 int main(int argc, char *argv[])
 {
-    pthread_t thid;
-    
-    pthread_private_val = 3;
-    printf("line: %d pthread_private_val = %d\n", __LINE__, pthread_private_val);
-
-    printf("main thread id = %ld\n", pthread_self());
-    sleep(1);
-
-    pthread_create(&thid, NULL, thread_func, NULL);
-
-    pthread_exit(NULL);
-//    return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
