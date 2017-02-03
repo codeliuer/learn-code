af322cfd32738f7b70f2a4d7898459381cd96a40
liuwei <18682303408@163.com>
2017-02-04 00:38:06 +0800

    ubuntu Sat Feb  4 00:38:06 HKT 2017

--- linux-code/unix_env/thread_attr/errno.c
+++ linux-code/unix_env/thread_attr/errno.c
@@ -11,13 +11,13 @@
 
 static int private_val;
 
-static pthread_key_t thread_key;
+static pthread_key_t process_key;
 static pthread_once_t once = PTHREAD_ONCE_INIT;
 
 
 static void create_only_key(void)
 {
-    pthread_key_create(&thread_key, free);
+    pthread_key_create(&process_key, free);
 }
 
 static int *__private_localtion(void)
@@ -25,7 +25,9 @@ static int *__private_localtion(void)
     void *value = NULL;
 
     pthread_once(&once, create_only_key);
-    if (pthread_getspecific(process_key))
+    if ((value = pthread_getspecific(process_key)) == NULL)
+    {
+    }
 }
 
 
