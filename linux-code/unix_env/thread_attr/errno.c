f3be1ba683f4ff1deeaad64e8a5e39503938fdc3
liuwei <18682303408@163.com>
2017-02-04 00:36:05 +0800

    ubuntu Sat Feb  4 00:36:05 HKT 2017

--- linux-code/unix_env/thread_attr/errno.c
+++ linux-code/unix_env/thread_attr/errno.c
@@ -15,6 +15,11 @@ static pthread_key_t thread_key;
 static pthread_once_t once = PTHREAD_ONCE_INIT;
 
 
+static void create_only_key(void)
+{
+    pthread_key_create(&thread_key, free);
+}
+
 static int *__private_localtion(void)
 {
     pthread_once(&once, create_only_key);
