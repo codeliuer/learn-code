2b3fa881b5d30710ca8a392c8c0ba2a9b1230e51
liuwei <18682303408@163.com>
2017-02-07 03:00:05 +0800

    ubuntu Tue Feb  7 03:00:05 HKT 2017

---
+++ linux-code/cplusplus/01/singleton.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+class A
+{
+public:
+    static A *Instance(void)
+    {
+        pthread_once(&m_once, thread_once);
+    }
+private:
+    A *m_pa;
+    pthread_once_t m_once;
+    pthread_key_t m_key;
+private:
+    A(void){}
+    A(const A&){}
+    A& operator=(const A&){}
+
+    ~A(void){}
+
+    void thread_once(void)
+    {
+        pthread_key_create(&m_key, delete);
+    }
+};
+
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
