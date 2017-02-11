81851ecc057ce22c56c0271272b255ef8ab14297
liuwei <18682303408@163.com>
2017-02-11 15:31:06 +0800

    ubuntu Sat Feb 11 15:31:06 HKT 2017

--- mac-code/unix_env/13/daemon_process.c
+++ mac-code/unix_env/13/daemon_process.c
@@ -6,5 +6,13 @@
 
 int main(int argc, char *argv[])
 {
+    pid_t pid;
+
+    if ((pid = fork()) < 0)
+    {
+        fprintf(stderr, "fork failure\n");
+        return EXIT_SUCCESS;    
+    }
+
     return EXIT_SUCCESS;
 }
