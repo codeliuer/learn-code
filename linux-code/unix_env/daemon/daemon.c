bc9402684030fd305272cd320aed3910a4935494
liuwei <18682303408@163.com>
2017-02-11 18:10:42 +0800

    ubuntu Sat Feb 11 18:10:42 HKT 2017

--- linux-code/unix_env/daemon/daemon.c
+++ linux-code/unix_env/daemon/daemon.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <syslog.h>
 
 #include <unistd.h>
 
 
 int main(int argc, char *argv[])
 {
+    const char *cmd =  "daemon test";
+
     daemon(0, 0);
 
-    printf("asdfadsfasdfas\n");
+    opensys(cmd, );
 
     return EXIT_SUCCESS;
 }
--- mac-code/unix_env/13/daemon_process.c
+++ mac-code/unix_env/13/daemon_process.c
@@ -77,5 +77,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
+    sleep(1000);
+
     return EXIT_SUCCESS;
 }
