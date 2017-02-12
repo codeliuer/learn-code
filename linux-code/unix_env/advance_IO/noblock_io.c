8c0f177d08946d7e99f0df89aa21b92ff0c3394e
liuwei <18682303408@163.com>
2017-02-12 21:28:06 +0800

    ubuntu Sun Feb 12 21:28:06 HKT 2017

--- linux-code/unix_env/advance_IO/noblock_io.c
+++ linux-code/unix_env/advance_IO/noblock_io.c
@@ -1,3 +1,10 @@
 /***********************************************************
- *
+ * block IO write
  ***********************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <fcntl.h>
