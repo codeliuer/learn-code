e4be9c0d9535c0dd08c83e9611ad65fb2b4d4bdb
liuwei <18682303408@163.com>
2017-02-17 03:00:09 +0800

    ubuntu Fri Feb 17 03:00:09 HKT 2017

--- linux-code/unix_env/advance_IO/test_lock.c
+++ linux-code/unix_env/advance_IO/test_lock.c
@@ -6,8 +6,30 @@
 #include <fcntl.h>
 
 
+#define readable_lock(fd, whence, start, len)       \
+    (test_lock((fd), F_RDLCK, (whence), (start), (len)))
+
+#define writeable_lock(fd, whence, start, len)      \
+    (test_lock((fd), F_WRLOCK, (whence), (start), (len)))
+
+
 static int test_lock(int fd, int type, int whence, int start, int len)
 {
+    struct flock flock;
+
+    flock.l_type = type;
+    flock.l_whence = whence;
+    flock.l_start = start;
+    flock.l_len = len;
+
+    fcntl(fd, F_GETLK, &flock);
+
+    if (flock.l_type == F_UNLCK)
+    {
+        return 0;
+    }
+
+    return flock.l_pid;
 }
 
 
