493fbfb604cf2f6d53627ad7f9fe500cf8518a81
liuwei <18682303408@163.com>
2017-02-20 00:57:05 +0800

    ubuntu Mon Feb 20 00:57:05 HKT 2017

---
+++ linux-code/unix_env/advance_IO/force.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+
+#define FORCE_FILE      stderr.out
+
+
+int main(int argc, char *argv[])
+{
+
+
+    return EXIT_SUCCESS;
+}
