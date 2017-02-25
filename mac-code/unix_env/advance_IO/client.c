fc72242589f9065ae542eebee506a88de15c90b6
18682303408@163.com <18682303408@163.com>
2017-02-26 03:31:05 +0800

    bogon Sun Feb 26 03:31:05 CST 2017

--- mac-code/unix_env/advance_IO/client.c
+++ mac-code/unix_env/advance_IO/client.c
@@ -10,6 +10,14 @@
 int main(int argc, char *argv[])
 {
     int sockfd = 0;
+    struct sockaddr_in addr = {0};
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        fprintf(stderr, "socket failure\n");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
