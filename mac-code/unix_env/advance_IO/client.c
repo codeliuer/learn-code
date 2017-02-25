9847671bd7d610130d8ff38d86d03d34e56e90e2
18682303408@163.com <18682303408@163.com>
2017-02-26 03:33:05 +0800

    bogon Sun Feb 26 03:33:05 CST 2017

--- mac-code/unix_env/advance_IO/client.c
+++ mac-code/unix_env/advance_IO/client.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
     int retcode = 0;
     int sockfd = 0;
     struct sockaddr_in addr = {0};
+    char buffer[BUFSIZ] = "";
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -24,7 +25,16 @@ int main(int argc, char *argv[])
     addr.sin_port = htons(2222);
     addr.sin_addr.s_addr = inet_addr("10.211.55.10");
 
+    retcode = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    if (retcode < 0)
+    {
+        fprintf(stderr, "connect failure\n");
+        return EXIT_FAILURE;
+    }
 
+    while (1)
+    {
+    }
 
     return EXIT_SUCCESS;
 }
