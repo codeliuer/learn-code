20d3dd2e5afe12c1da53e0baed34ed4346341a25
liuwei <18682303408@163.com>
2017-02-27 00:29:05 +0800

    ubuntu Mon Feb 27 00:29:05 HKT 2017

--- linux-code/unix_env/advance_IO/server/server.c
+++ linux-code/unix_env/advance_IO/server/server.c
@@ -16,6 +16,9 @@
 int main(int argc, char *argv[])
 {
     int sockfd = 0;
+    struct sockaddr_in seraddr;
+    struct sockaddr_in cliaddr;
+    socklen_t clilen = sizeof(cliaddr);
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
@@ -23,6 +26,8 @@ int main(int argc, char *argv[])
         fprintf(stderr, "socket failure\n");
         return EXIT_FAILURE;
     }
+
+
 
     return EXIT_SUCCESS;
 }
