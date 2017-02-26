870567c17aca19405cef5c54579a1d95bfa5cd45
liuwei <18682303408@163.com>
2017-02-27 00:38:04 +0800

    ubuntu Mon Feb 27 00:38:04 HKT 2017

--- linux-code/unix_env/advance_IO/server/server.c
+++ linux-code/unix_env/advance_IO/server/server.c
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
 
         if (pollfd[0].events == POLLIN)
         {
-
+            clifd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen);
         }
     }
 
