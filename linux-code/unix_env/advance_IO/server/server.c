7b1abdcb0ed552bf4ef572c02b4c6c8e72f5b031
liuwei <18682303408@163.com>
2017-02-27 00:28:05 +0800

    ubuntu Mon Feb 27 00:28:05 HKT 2017

--- linux-code/unix_env/advance_IO/server/server.c
+++ linux-code/unix_env/advance_IO/server/server.c
@@ -15,5 +15,14 @@
 
 int main(int argc, char *argv[])
 {
+    int sockfd = 0;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        fprintf(stderr, "socket failure\n");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
