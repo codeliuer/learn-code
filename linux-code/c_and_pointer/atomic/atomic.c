3b0b979901e9db7f4ce9dc10702a846f20b80ca6
liuwei <18682303408@163.com>
2017-02-28 00:04:10 +0800

    ubuntu Tue Feb 28 00:04:10 HKT 2017

--- linux-code/c_and_pointer/atomic/atomic.c
+++ linux-code/c_and_pointer/atomic/atomic.c
@@ -5,5 +5,16 @@
 
 int main(int argc, char *argv[])
 {
+    int mutex = 0;
+    int lock = 0;
+    int unlock = 1;
+    int count = 0;
+
+    __sync_bool_compare_and_swap(&mutex, lock, 1);
+    count++;
+    __sync_bool_compare_and_swap(&mutex, unlock, 1);
+
+    printf("count = %d\n", count);
+
     return EXIT_SUCCESS;
 }
