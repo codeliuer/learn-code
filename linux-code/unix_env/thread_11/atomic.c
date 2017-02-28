8c6c96b8c47cacac41f8bbf887a3a6606bce91e6
liuwei <18682303408@163.com>
2017-03-01 00:10:08 +0800

    ubuntu Wed Mar  1 00:10:08 HKT 2017

--- linux-code/unix_env/thread_11/atomic.c
+++ linux-code/unix_env/thread_11/atomic.c
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <unistd.h>
+#include <pthread.h>
+
+
+static int count = 0;
+
 
 int main(int argc, char *argv[])
 {
+    int i = 0;
+    pthread_t tid;
+
+    for (i = 0; i < 10; i++)
+    {
+    }
+
     return EXIT_SUCCESS;
 }
