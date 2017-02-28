5935a418f13541de275563008bfcbfde2aae4dfc
liuwei <18682303408@163.com>
2017-03-01 00:24:07 +0800

    ubuntu Wed Mar  1 00:24:07 HKT 2017

--- linux-code/unix_env/thread_11/__sync_bool_compare_swap.c
+++ linux-code/unix_env/thread_11/__sync_bool_compare_swap.c
@@ -5,6 +5,13 @@
 
 int main(int argc, char *argv[])
 {
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 0;
+
+    d = __sync_bool_compare_and_swap(&a, b, c);
+    printf("d = %d\n", d);
 
     return EXIT_SUCCESS;
 }
