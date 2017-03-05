0eac5f86944c486289cb23b7570b0a7d55e8f251
18682303408@163.com <18682303408@163.com>
2017-03-06 06:00:06 +0800

    bogon Mon Mar  6 06:00:05 CST 2017

--- mac-code/network/test/client.c
+++ mac-code/network/test/client.c
@@ -9,7 +9,9 @@
 
 int main(int argc, char *argv[])
 {
+    int ret = 0;
     int sockfd = 0;
+    struct sockaddr_in cliaddr;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -17,6 +19,12 @@ int main(int argc, char *argv[])
         fprintf(stderr, "socket failure\n");
         return EXIT_FAILURE;
     }
+
+    cliaddr.sin_family = AF_INET;
+    cliaddr.sin_port = htons(2222);
+    cliaddr.sin_addr.s_addr = inet_addr("10.211.55.2");
+
+    ret = connect(sockfd, );
 
     return EXIT_SUCCESS;
 }
