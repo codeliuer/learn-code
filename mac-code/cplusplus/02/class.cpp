22a3583e14a16d921b6f4c6c3e018c2f50cb2b22
18682303408@163.com <18682303408@163.com>
2017-03-14 00:38:06 +0800

    bogon Tue Mar 14 00:38:06 CST 2017

--- mac-code/cplusplus/02/class.cpp
+++ mac-code/cplusplus/02/class.cpp
@@ -24,11 +24,26 @@ public:
 
 class B : public A
 {
+public:
+    B()
+    {
+        cout << "B" << endl;
+    }
+    ~B()
+    {
+        cout << "~B" << endl;
+    }
+
+    virtual void func(void)
+    {
+        cout << "B func()" << endl;
+    }
 };
 
 
 int main(int argc, char *argv[])
 {
+    A *pa = new B();
 
     return EXIT_SUCCESS;
 }
