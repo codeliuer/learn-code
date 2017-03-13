2b5219fc9d417af2a7d5480322aa0de9ec57ee8c
18682303408@163.com <18682303408@163.com>
2017-03-14 00:37:08 +0800

    bogon Tue Mar 14 00:37:08 CST 2017

--- mac-code/cplusplus/02/class.cpp
+++ mac-code/cplusplus/02/class.cpp
@@ -4,6 +4,29 @@
 using namespace std;
 
 
+class A
+{
+public:
+    A()
+    {
+        cout << "A" << endl;
+    }
+    ~A()
+    {
+        cout << "~A" << endl;
+    }
+
+    virtual void func(void)
+    {
+        cout << "A func()" << endl;
+    }
+};
+
+class B : public A
+{
+};
+
+
 int main(int argc, char *argv[])
 {
 
