a39d00ed761b547eaf546e3b305b586329b98699
18682303408@163.com <18682303408@163.com>
2017-03-19 15:13:09 +0800

    bogon Sun Mar 19 15:13:09 CST 2017

--- linux-code/c_and_pointer/19/full_perm.cpp
+++ linux-code/c_and_pointer/19/full_perm.cpp
@@ -4,6 +4,7 @@
 
 static void perm(int *arr, size_t size)
 {
+
 }
 
 
@@ -11,7 +12,7 @@ int main(int argc, char *argv[])
 {
     int arr[] = {1, 2, 3, 4, 5, 6,};
 
-    perm(arr, sizeof(arr)/sizeof(arr[0]));
+    perm(arr, 0, sizeof(arr)/sizeof(arr[0])-1);
 
     return EXIT_SUCCESS;
 }
