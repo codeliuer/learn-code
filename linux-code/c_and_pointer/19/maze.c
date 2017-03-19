6f32c02b0d86d23e20798af749d54a9277f61e4f
18682303408@163.com <18682303408@163.com>
2017-03-19 15:50:10 +0800

    bogon Sun Mar 19 15:50:10 CST 2017

--- linux-code/c_and_pointer/19/maze.c
+++ linux-code/c_and_pointer/19/maze.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define M           (10)
+#define N           (13)
 
-int arr[M+1][N+1] = {
+
+int arr[M+2][N+2] = {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
     {0, },
     {0, },
