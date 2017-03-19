1f7f55ca931d2310908191a4387e34236722d93b
18682303408@163.com <18682303408@163.com>
2017-03-19 18:00:08 +0800

    bogon Sun Mar 19 18:00:08 CST 2017

--- linux-code/c_and_pointer/19/maze.c
+++ linux-code/c_and_pointer/19/maze.c
@@ -33,14 +33,124 @@ static int top = -1;
 
 static void push(int x, int y)
 {
+    int i = ++top;
+
+    stack[i].x = x;
+    stack[i].y = y;
+
+//    printf("(%d, %d)", x, y);
 }
 
-static INDEX pop(void)
+static INDEX *pop(void)
 {
+    if (top == -1)
+    {
+        return NULL;
+    }
+
+    return &stack[top--];
 }
 
+
+static void show_path(void)
+{
+    INDEX *tmp = NULL;
+
+    tmp = pop();
+    if (tmp == NULL)
+    {
+        return;
+    }
+
+    show_path();
+    //printf("(%d, %d) ", tmp->x, tmp->y);
+}
+
+static int maze(void)
+{
+    int x = 1;
+    int y = 1;
+    INDEX *tmp = NULL;
+
+    while (x != M || y != N)
+    {
+        if (arr[x][y-1] != 1 && arr[x-1][y] != 1 
+            && arr[x][y+1] != 1 && arr[x+1][y] != 1)
+        {
+            arr[x][y] = 2;
+
+            if ((tmp = pop()) == NULL)
+            {
+                return -1;
+            }
+
+            x = tmp->x;
+            y = tmp->y;
+        }
+
+        push(x, y);
+        arr[x][y] = 2;
+        if (arr[x][y-1] == 1)
+        {
+            y -= 1;
+        }
+        else if (arr[x-1][y] == 1)
+        {
+            x -= 1;
+        }
+        else if (arr[x][y+1] == 1)
+        {
+            y += 1;
+        }
+        else
+        {
+            x += 1;
+        }
+    }
+
+    if (arr[M][N] == 1)
+    {
+        return 0;
+    }
+
+    return -1;
+}
+
+static void show_chess(void)
+{
+    int i = 0;
+    int j = 0;
+
+    printf("        ");
+    for (j = 0; j < N+2; j++)
+    {
+        printf("%4d", j);
+    }
+    printf("\n");
+    printf("\n");
+
+    for (i = 0; i < M+2; i++)
+    {
+        printf("%s%-2d", "      ", i);
+        for (j = 0; j < N+2; j++)
+        {
+            printf("%4d", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char *argv[])
 {
+//    if (maze() == 0)
+    {
+        printf("\n");
+        show_chess();
+        //show_path();
+        printf("\n");
+    }
+
+    printf("\n");
+
     return EXIT_SUCCESS;
 }
