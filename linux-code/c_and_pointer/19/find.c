ecf7fee236b5a5d52a69f8ef997ac21013a77003
18682303408@163.com <18682303408@163.com>
2017-03-20 03:46:05 +0800

    bogon Mon Mar 20 03:46:05 CST 2017

---
+++ linux-code/c_and_pointer/19/find.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static int arr[5][5] = 
+{
+    {1, 2, 3, 4, 5,},
+    {2, 3, 4, 5, 6,},
+    {3, 4, 5, 6, 7,},
+    {4, 5, 6, 7, 8,},
+    {5, 6, 7, 8, 9},
+}
+
+
+int main(int rgc, char *argv[])
+{
+    int n = 0;
+
+    n = 8;
+
+    return EXIT_SUCCESS;
+}
