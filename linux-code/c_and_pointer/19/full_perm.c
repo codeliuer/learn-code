30ee16df64d1b05c1cd5a304530bd3932d0fbc36
18682303408@163.com <18682303408@163.com>
2017-03-19 15:39:05 +0800

    bogon Sun Mar 19 15:39:05 CST 2017

--- linux-code/c_and_pointer/19/full_perm.c
+++ linux-code/c_and_pointer/19/full_perm.c
@@ -44,8 +44,7 @@ static void perm(int *arr, size_t start, size_t end)
 
 int main(int argc, char *argv[])
 {
-    //int arr[] = {1, 2, 3, 4, 5, 6,};
-    int arr[] = {1, 2, 3, 4, };
+    int arr[] = {1, 2, 3, 4, 5, 6,};
 
     perm(arr, 0, sizeof(arr)/sizeof(arr[0])-1);
     printf("num = %d\n", num);
