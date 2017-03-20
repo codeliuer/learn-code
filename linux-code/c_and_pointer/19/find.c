5d662aee19bf830016363d8dd0a7606bc3013793
18682303408@163.com <18682303408@163.com>
2017-03-21 00:18:09 +0800

    bogon Tue Mar 21 00:18:09 CST 2017

--- linux-code/c_and_pointer/19/find.c
+++ linux-code/c_and_pointer/19/find.c
@@ -8,42 +8,11 @@ static int arr[5][5] =
     {2, 3, 4, 5, 6,},
     {3, 4, 5, 6, 7,},
     {4, 5, 6, 7, 8,},
-    {5, 6, 7, 8, 9,},
+    {5, 6, 7, 10, 11,},
 };
 
 
 int main(int rgc, char *argv[])
 {
-    int n = 0;
-    int i = 1;
-    int j = 1;
-
-    n = 8;
-
-    while (i != 5 || j != 5)
-    {
-        if (arr[i][j] == n)
-        {
-            printf("find number (%d, %d)\n", i, j);
-        }
-        else if (arr[i][j] > n)
-        {
-            i += 1;
-        }
-        else
-        {
-            j += 1;
-        }
-    }
-
-    if (arr[i][j] == n)
-    {
-        printf("find number (%d, %d)\n", i, j);
-    }
-    else
-    {
-        printf("not find number, n = %d\n", n);
-    }
-
     return EXIT_SUCCESS;
 }
