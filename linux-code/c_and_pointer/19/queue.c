d69fb6ca1043d1cccabb385e9a8e4b400ca1562d
18682303408@163.com <18682303408@163.com>
2017-03-22 03:37:06 +0800

    bogon Wed Mar 22 03:37:06 CST 2017

---
+++ linux-code/c_and_pointer/19/queue.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define N           (8)
+
+
+char arr[N][N] = 
+{
+    {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',},
+    {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',},
+    {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',},
+    {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',},
+    {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',},
+    {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',},
+    {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',},
+    {'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',},
+};
+
+
+int main(int argc, char *argv[])
+{
+    char c = 'X';
+
+    return EXIT_SUCCESS;
+}
