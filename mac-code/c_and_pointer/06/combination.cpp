38eda4e0bed469d8efea005059f3a7b727bdd4ba
18682303408@163.com <18682303408@163.com>
2017-03-28 15:30:04 +0800

    bogon Tue Mar 28 15:30:04 CST 2017

---
+++ mac-code/c_and_pointer/06/combination.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+const int num[10] = {3,2,1,4,5,6,7,8,9,10};
+const int sum = 10;
+const int n=10;
+
+int main(int argc, char *argv[])
+{
+    int i,j;
+    int result[sum+1]={0};
+    int temp[sum+1]={0};
+
+    for(i=0; i<n; i++)
+    {
+          if(num[i]>sum)
+                 continue;
+            else
+                  {
+                         for(j=0; j<sum+1; j++)
+                                {
+                                        temp[j]=result[j];
+                                           }
+                            temp[0]=1;
+                              
+                               for(j=0;j<sum+1;j++)
+                                      {
+                                              if( (j+num[i])<=sum)
+                                                      {
+                                                               result[j+num[i]] += temp[j];
+                                                                   }
+                                                 }
+                                 }
+    }
+    cout<<result[sum];
+    return 0;
+}
