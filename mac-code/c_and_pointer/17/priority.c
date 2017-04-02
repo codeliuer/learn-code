0dddafaacab12d04c70dfc735332a8dc261ad399
18682303408@163.com <18682303408@163.com>
2017-04-03 03:00:05 +0800

    bogon Mon Apr  3 03:00:05 CST 2017

---
+++ mac-code/c_and_pointer/17/priority.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+
+
+#define QUEUE_MAX           (100)
+
+
+typedef struct NODE
+{
+    int             value;
+    struct NODE     *left;
+    struct NODE     *right;
+} NODE;
+
+typedef struct MAXQUE
+{
+    int             nums;
+    int             max;
+    NODE            *queue;
+} MAXQUE;
+
+static MAXQUE queue = {1, QUEUE_MAX, NULL};
+
+static void push(int value)
+{
+}
+
+static int pop(void)
+{
+    int index = queue.nums--;
+    int value = queue.queue[1].value;
+
+    queue.queue[1].value = queue.queue[index].value;
+}
+
+static int init_queue(void)
+{
+    queue.queue = (NODE *)malloc(sizeof(NODE) * (QUEUE_MAX+1));
+    if (queue.queue == NULL)
+    {
+        fprintf(stderr, "%s[%d] malloc failure\n", __func__, __LINE__);
+        return -1;
+    }
+
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    
+
+    return EXIT_SUCCESS;
+}
