5ebc0d2b7dbd4a34a6c1091ec46e2a29c436d73f
18682303408@163.com <18682303408@163.com>
2017-04-07 15:45:05 +0800

    macos Fri Apr  7 15:45:05 CST 2017

--- mac-code/c_and_pointer/01/static.c
+++ mac-code/c_and_pointer/01/static.c
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 
 
+static void static_var(void)
+{
+    static int var = 1000;
+
+    printf("static_var %d\n", var);
+}
+
 int main(int argc, char *argv[])
 {
+    static int var = 100;
+
+    static_var();
+
+    printf("main %d\n", var);
+
     static_var();
 
     return EXIT_SUCCESS;
