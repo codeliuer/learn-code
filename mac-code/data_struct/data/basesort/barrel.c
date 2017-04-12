dda279b2925de721877a7c8c5662c0dcf997ba7e
18682303408@163.com <18682303408@163.com>
2017-04-12 12:09:10 +0800

    bogon Wed Apr 12 12:09:10 CST 2017

--- mac-code/data_struct/data/basesort/barrel.c
+++ mac-code/data_struct/data/basesort/barrel.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <time.h>
 
 
 static int *bucket[10] = {0};
@@ -9,7 +10,10 @@ static int *bucket[10] = {0};
 
 int main(int argc, char *argv[])
 {
+    int i = 0;
+    int nums = 0;
 
+    scanf("%d", &nums);
 
     return EXIT_SUCCESS;
 }
