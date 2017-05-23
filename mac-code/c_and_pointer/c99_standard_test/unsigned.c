bc261ce41ec05551b5e75dec4cd2bb5bb340b41d
liuwei <18682303408@163.com>
2017-05-24 00:51:08 +0800

    ubuntu Wed May 24 00:51:08 HKT 2017

--- mac-code/c_and_pointer/c99_standard_test/unsigned.c
+++ mac-code/c_and_pointer/c99_standard_test/unsigned.c
@@ -5,7 +5,9 @@
 
 int main(int argc, char *argv[])
 {
-    unsigned char arr[] = {0x01};
+    unsigned char arr[] = {0x01, 0x02, 0x03, 0x04};
+
+    printf("%ld\n", sizeof(arr));
 
     return EXIT_SUCCESS;
 }
