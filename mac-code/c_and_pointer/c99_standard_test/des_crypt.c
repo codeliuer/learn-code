af65423d3fbaf72876cfb9f6aa9989fe2a242582
liuwei <18682303408@163.com>
2017-05-24 00:00:06 +0800

    ubuntu Wed May 24 00:00:06 HKT 2017

--- mac-code/c_and_pointer/c99_standard_test/des_crypt.c
+++ mac-code/c_and_pointer/c99_standard_test/des_crypt.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <setjmp.h>
 
 #include <unistd.h>
 #include <fcntl.h>
 #include <rpc/des_crypt.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
+
+#define OUTFILE         "des_outfile"
+
+
+static jmp_buf env;
 
 
 void des_encrypt(const char *key, char *data, int len)
@@ -34,28 +41,117 @@ static void show(const char *data, int len)
     printf("\n");
 }
 
+static int openfile(const char *filename)
+{
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0)
+    {
+        fprintf(stderr, "open %s failure", filename);
+        longjmp(env, 4);
+    }
+
+    return fd;
+}
+
+static long filelen(int fd)
+{
+    struct stat stat;
+
+    fstat(fd, &stat);
+
+    return stat.st_size;
+}
+
+void *dataspace(long int len)
+{
+    void *data = malloc(len);
+    if (data == NULL)
+    {
+        fprintf(stderr, "malloc failure\n");
+        longjmp(env, 6);
+    }
+
+    return data;
+}
+
+static void closefile(int fd)
+{
+    close(fd);
+}
+
+static void get_input_info(unsigned char **data, long int *plen, const char *filename)
+{
+    int fd = 0;
+    fd = openfile(filename);   
+    *plen = filelen(fd);
+    *data = dataspace(*plen+8);
+
+    if (read(fd, *data, *plen) < 0)
+    {
+        fprintf(stderr, "read file failure\n");
+        free(*data), *data = NULL;
+        longjmp(env, 3);
+    }
+
+    closefile(fd);
+}
+
+static int create_outfile(const char *filename)
+{
+    int fd = 0;
+
+    unlink(filename);
+
+    fd = open(filename, O_CREAT|O_TRUNC|O_RDWR, 0777);
+    if (fd < 0)
+    {
+        fprintf(stderr, "open %s failure", filename);
+        longjmp(env, 1);
+    }
+
+    return fd;
+}
+
+static void set_output_file(unsigned char *data, long int filelen)
+{
+    int fd = 0;
+
+    fd = create_outfile("outfile.ko");
+    if (write(fd, data, filelen) != filelen)
+    {
+        fprintf(stderr, "write failure\n");
+        free(data), data = NULL;
+
+        longjmp(env, 2);
+    }
+
+    free(data), data = NULL;
+}
+
 int main(int argc, char *argv[])
 {
-    int fin, fout1, fout2;
-    const char key[] = "12345678";
-    char *data = NULL;
-    struct stat st;
+    int ret = 0;
+    unsigned char *pdata = NULL;
+    long int filelen = 0;
+    const char *key = "12345678";
 
-    fin = open("pragma.c", O_RDONLY);
-    if (fin < 0)
+    if (argc < 2)
     {
-        fprintf(stderr, "open pragma.c failure\n");
+        fprintf(stderr, "Usage: ./a.out filename1, ...\n");
         return EXIT_FAILURE;
     }
 
-    fstat(fin, &st);
-    data = (char *)malloc(st.st_size + 8);
-
-    read(fin, data, st.st_size);
-
-    show(data, 10);
-    des_encrypt(key, data, st.st_size);
-    show(data, 10);
+    if ((ret = setjmp(env)) == 0)
+    {
+        get_input_info(&pdata, &filelen, argv[1]);
+        des_encrypt(key, pdata, filelen);
+        set_output_file(pdata, filelen);
+    }
+    else
+    {
+        fprintf(stderr, "%d error\n", ret);
+        return EXIT_FAILURE;
+    }
     
     return EXIT_SUCCESS;
 }
