d8effab709171fa8f922f7832d0b54ab1d8f5772
liuwei <18682303408@163.com>
2017-06-18 00:12:24 +0800

    ubuntu Sun Jun 18 00:12:24 HKT 2017

--- linux-code/unix_env/IPC/open_fifo.c
+++ linux-code/unix_env/IPC/open_fifo.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
 
+extern int errno;
+
 int main(int argc, char *argv[])
 {
     int fd = 0;
 
-    fd = open("/Users/weiliu/learn-code/test.text",);
+    errno = 0;
+
+    fd = open("/Users/weiliu/learn-code/test.text", O_WRONLY);
+    if (fd < 0)
+    {
+        perror(errno);
+    }
+    
 
     return EXIT_SUCCESS;
 }
