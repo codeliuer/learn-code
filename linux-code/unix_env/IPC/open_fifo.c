b7aaddaccd32f554d064db5eae787b6571518c2f
liuwei <18682303408@163.com>
2017-06-18 00:16:06 +0800

    ubuntu Sun Jun 18 00:16:06 HKT 2017

--- linux-code/unix_env/IPC/open_fifo.c
+++ linux-code/unix_env/IPC/open_fifo.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
     int fd = 0;
 
-    fd = open("test.text", O_WRONLY);
+    fd = open("test.txt", O_WRONLY);
     if (fd < 0)
     {
         perror("open failure: ");
