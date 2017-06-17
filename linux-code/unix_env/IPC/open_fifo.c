8503a26b30f44be2e31433112e1ed0f6ff83363f
liuwei <18682303408@163.com>
2017-06-18 00:08:04 +0800

    ubuntu Sun Jun 18 00:08:04 HKT 2017

--- linux-code/unix_env/IPC/open_fifo.c
+++ linux-code/unix_env/IPC/open_fifo.c
@@ -1,2 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
