7090ac22996c4d40b9feaadd7aeb7b277fb36591
liuwei <18682303408@163.com>
2017-06-17 18:55:02 +0800

    ubuntu Sat Jun 17 18:55:02 HKT 2017

---
+++ linux-code/unix_env/IPC/mkfifoat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char *argv[])
+{
+    int fd = 0;
+    int fifofd = 0;
+    DIR *dir = NULL;
+
+    dir = opendir("/Users/weiliu");
+    if (dir == NULL)
+    {
+        fprintf(stderr, "opendir failure\n");
+        return EXIT_FAILURE;
+    }
+
+    fd = dirfd(dir);
+    if (fd < 0)
+    {
+    }
+
+    return EXIT_SUCCESS;
+}
