e2e38e959ae8856edd2cd649ee3b02c0205c7b32
liuwei <18682303408@163.com>
2017-06-21 00:54:04 +0800

    ubuntu Wed Jun 21 00:54:04 HKT 2017

--- linux-code/unix_env/IPC/apue.c
+++ linux-code/unix_env/IPC/apue.c
@@ -11,6 +11,15 @@ int main(int argc, char *argv[])
     int n = 0;
     int fd[2];
     pid_t pid;
+    char *pager, *argv0;
+    char line[MAXLINE];
+    FILE *fp;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: ./a.out filename\n");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_FAILURE;
 }
