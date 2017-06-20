5c33bf15ae6ba5b52b7557ab9cb0dc0f0c13aeb3
liuwei <18682303408@163.com>
2017-06-21 00:23:04 +0800

    ubuntu Wed Jun 21 00:23:04 HKT 2017

--- linux-code/unix_env/IPC/more.c
+++ linux-code/unix_env/IPC/more.c
@@ -9,5 +9,14 @@
 
 int main(int argc, char *argv[])
 {
+    int fds[2] = {0};
+    pid_t pid;
+
+    if (pipe(fds) < 0)
+    {
+        perror("pipe failure: ");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
