26721da780542a772e3897eb706cf0189bb8eacf
liuwei <18682303408@163.com>
2017-06-21 00:53:05 +0800

    ubuntu Wed Jun 21 00:53:05 HKT 2017

---
+++ linux-code/unix_env/IPC/apue.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+int main(int argc, char *argv[])
+{
+    int n = 0;
+    int fd[2];
+    pid_t pid;
+
+    return EXIT_FAILURE;
+}
