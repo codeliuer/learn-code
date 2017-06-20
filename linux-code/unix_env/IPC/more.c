ff9b84997b1c165497a1119cd2d164bda96c1a7d
liuwei <18682303408@163.com>
2017-06-21 00:22:07 +0800

    ubuntu Wed Jun 21 00:22:07 HKT 2017

---
+++ linux-code/unix_env/IPC/more.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+
+int main(int argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
