0df98456793f6c4bb237f90e648ca9ee21c0c547
liuwei <18682303408@163.com>
2017-06-22 00:20:06 +0800

    ubuntu Thu Jun 22 00:20:06 HKT 2017

---
+++ linux-code/unix_env/IPC/buffer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[])
+{
+    pid_t pid;
+
+    setvbuf();
+
+    return EXIT_SUCCESS;
+}
