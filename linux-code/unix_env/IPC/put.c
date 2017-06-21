c7e7de581e263c61855e6a8c6a69dab2a7c07c1e
liuwei <18682303408@163.com>
2017-06-22 00:26:05 +0800

    ubuntu Thu Jun 22 00:26:05 HKT 2017

--- linux-code/unix_env/IPC/put.c
+++ linux-code/unix_env/IPC/put.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 
 int main(int argc, char *argv[])
 {
     char buffer[BUFSIZ] = "";
 
-    read(STDIN_FILENO, )
+    while (1){
+    read(STDIN_FILENO, buffer, sizeof(buffer));
+    write(STDOUT_FILENO, buffer, strlen(buffer));
+    }
 
     return EXIT_SUCCESS;
 }
