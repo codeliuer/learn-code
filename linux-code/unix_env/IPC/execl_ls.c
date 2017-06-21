5a96cdc41446f64dbdf7a28ab5e5c109d9f1f869
liuwei <18682303408@163.com>
2017-06-22 00:11:05 +0800

    ubuntu Thu Jun 22 00:11:05 HKT 2017

---
+++ linux-code/unix_env/IPC/execl_ls.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+
+
+int main(int argc, char *argv[])
+{
+    execl("/usr/bin/who", "bin/ls", (char *)0);
+
+    return EXIT_SUCCESS;
+}
