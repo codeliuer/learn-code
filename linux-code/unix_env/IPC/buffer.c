7f9fc48501072c3a54f4de64736084f33b52acd9
liuwei <18682303408@163.com>
2017-06-22 00:32:04 +0800

    ubuntu Thu Jun 22 00:32:04 HKT 2017

--- linux-code/unix_env/IPC/buffer.c
+++ linux-code/unix_env/IPC/buffer.c
@@ -9,8 +9,11 @@ int main(int argc, char *argv[])
     pid_t pid;
     char buffer[BUFSIZ] = "";
 
-    setvbuf(stdin, buffer, _IOFBF, BUFSIZ);
-    setvbuf(stdout, buffer, _IONBF, BUFSIZ);
+//    setvbuf(stdin, buffer, _IOFBF, BUFSIZ);
+//    setvbuf(stdout, buffer, _IONBF, BUFSIZ);
+
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
 
     if ((pid = fork()) < 0)
     {
