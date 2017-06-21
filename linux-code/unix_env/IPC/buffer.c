04840e1548bfedd21e56bbb41e5cd235df6425e0
liuwei <18682303408@163.com>
2017-06-22 00:21:04 +0800

    ubuntu Thu Jun 22 00:21:04 HKT 2017

--- linux-code/unix_env/IPC/buffer.c
+++ linux-code/unix_env/IPC/buffer.c
@@ -8,7 +8,20 @@ int main(int argc, char *argv[])
 {
     pid_t pid;
 
-    setvbuf();
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    if ((pid = fork()) < 0)
+    {
+        perror("fork failure: ");
+        return EXIT_FAILURE;
+    }
+    else if (pid == 0)
+    {
+    }
+    else
+    {
+    }
 
     return EXIT_SUCCESS;
 }
