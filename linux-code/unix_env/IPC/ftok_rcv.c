814b473d2946a046e0266788f7e1efe2f431c433
liuwei <18682303408@163.com>
2017-06-23 00:30:10 +0800

    ubuntu Fri Jun 23 00:30:10 HKT 2017

---
+++ linux-code/unix_env/IPC/ftok_rcv.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+
+int main(int argc, char *argv[])
+{
+    int msgid;
+
+    return EXIT_SUCCESS;
+}
