98b446e85854cec11f0f6885ba7e143c40d2ab5f
liuwei <18682303408@163.com>
2017-06-24 18:57:15 +0800

    ubuntu Sat Jun 24 18:57:15 HKT 2017

---
+++ linux-code/unix_env/IPC/process_sync/sync.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+
+
+int TELL_WAIT()
+{
+}
+
+int WAIT_PARENT()
+{
+}
+
+int WAIT_CHILD()
+{
+}
+
+int TELL_PARENT()
+{
+}
+
+int TELL_CHILD()
+{
+}
