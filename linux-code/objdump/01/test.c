532f66122f92f8d428373bc807f96c27684fa461
liuwei <18682303408@163.com>
2017-07-09 21:49:07 +0800

    ubuntu Sun Jul  9 21:49:07 HKT 2017

--- linux-code/objdump/01/test.c
+++ linux-code/objdump/01/test.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     printf(" *******\n");
     printf("  *****\n");
     printf("    *\n");
-    printf("%d\n", 1633028);
+    putc(1633028);
 
     return EXIT_SUCCESS;
 }
