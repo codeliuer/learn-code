142fd949136d3b418f6d390be5263e28bb51b533
liuwei <18682303408@163.com>
2017-07-09 21:58:05 +0800

    ubuntu Sun Jul  9 21:58:05 HKT 2017

--- linux-code/objdump/01/test.c
+++ linux-code/objdump/01/test.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 
 int main(int argc, char *argv[])
 {
-    printf("  *   *\n");
-    printf(" *******\n");
-    printf("  *****\n");
-    printf("    *\n");
     printf("1633027\n");
 
     return EXIT_SUCCESS;
